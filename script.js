2c06309fab08e34aee7c9ca3a791ee928262a5e5
Amber Forrest <amber4est@gmail.com>
2025-07-28 11:43:00 -0400

    Added a simple form with name and email to handle form events (submit button and reset button)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Mouse Event
+//Mouse Events
 const mouseButton=document.getElementById("hover-btn");
 const mouseMessage=document.getElementById("mouse-btn");
 
@@ -14,7 +14,7 @@ mouseButton.addEventListener("click", function() {
     mouseMessage.textContent="Button was clicked!";
 });
 
-//Keyboard Event
+//Keyboard Events
 const keyboardEvent=document.getElementById("keyboard-events");
 const keyboardInput=document.getElementById("keyboard-input");
 
@@ -24,4 +24,25 @@ keyboardEvent.addEventListener("keydown", function(keys) {
 
 keyboardEvent.addEventListener("keyup", function(keys) {
     keyboardInput.textContent=`Key Released: ${keys.key}, Text Inputted: ${keys.target.value}`;
+});
+
+//Form Events
+const formEvents=document.getElementById("form-events");
+const formInput=document.getElementById("form-input");
+
+formEvents.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const nameInput=document.getElementById("name");
+    const name=nameInput.value;
+
+    const emailInput=document.getElementById("email");
+    const email=emailInput.value;
+
+    formInput.textContent=`Form has been submitted! Name: ${name} Email: ${email}`;
+});
+
+formEvents.addEventListener('reset', function() {
+    formEvents.reset();
+    formInput.textContent="Form has been reset!";
 });
